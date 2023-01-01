c638540c32d455cc776f2928c22c9b5cc6ca1884
FrancioXavier <103940118+FrancioXavier@users.noreply.github.com>
2023-01-01 10:59:46 -0300

    types os objects and arrays

--- app.ts
+++ app.ts
@@ -1,5 +1,23 @@
-let a: string = 'a'
-let b: number = 2
-let c: boolean = true
+interface Pessoa{
+    name: string,
+    age: number,
+    profession?: string // campo opcional
+}//instanciando o objeto Pessoa
 
-let d: number | string
+const francio: Pessoa = {
+    name: "Francio",
+    age: 19
+}
+const clara: Pessoa = {
+    name: "Clara",
+    age: 18
+}
+
+const listaPessoas: Array<Pessoa> = [
+    francio,
+    clara
+] //O tipo passado é o tipo Array em que os campos são objetos do tipo pessoa
+
+//OU
+
+const listaNum: number[] = [1, 2, 3]
